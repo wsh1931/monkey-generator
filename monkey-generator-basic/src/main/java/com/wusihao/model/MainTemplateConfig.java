c44f4b7ab529c4c6a9911eb594e93662b96dd4d0
wsh <1931443283@qq.com>
2025-01-30 12:10:00 +0800

    引入 FreeMarker 完成动态文件生成

---
+++ monkey-generator-basic/src/main/java/com/wusihao/model/MainTemplateConfig.java
@@ -0,0 +1,25 @@
+package com.wusihao.model;
+
+import lombok.Data;
+
+/**
+ * 动态模版配置
+ */
+@Data
+public class MainTemplateConfig {
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop;
+
+    /**
+     * 作者注释
+     */
+    private String author = "wusihao";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "输出结果";
+}
