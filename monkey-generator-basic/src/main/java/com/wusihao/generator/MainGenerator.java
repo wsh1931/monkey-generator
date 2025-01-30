c5cc738d8722ffa2bd8b41f24830e5218b447ab8
wsh <1931443283@qq.com>
2025-01-30 12:25:47 +0800

    完成动静结合代码生成

---
+++ monkey-generator-basic/src/main/java/com/wusihao/generator/MainGenerator.java
@@ -0,0 +1,46 @@
+package com.wusihao.generator;
+
+import com.wusihao.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 核心生成器
+ */
+public class MainGenerator {
+
+    /**
+     * 生成
+     * 
+     * @param model 数据模型
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir") + File.separator + "monkey-generator-basic";
+        // 整个项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+        // 输入路径
+        String inputPath = new File(parentFile, "monkey-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+        // 生成静态文件
+        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/wusihao/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+
+
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("wusihao");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerate(mainTemplateConfig);
+    }
+}
+
